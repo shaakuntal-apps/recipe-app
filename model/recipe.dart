b7e31752f5096caf85d96ee450133a1c229f0f9a
Aditya B Nawandar <nawandaraditya60@gmail.com>
2021-09-19 21:17:43 +0530

    added initial code

--- model/recipe.dart
+++ model/recipe.dart
@@ -1,1 +1,30 @@
+class Recipe {
+  //final String id;
+  final String recipeName;
+  final String imageUrl;
+  final int prepTime;
+  final Complexity complexity;
+  final Affordability affordability;
+  final List<String> ingredients;
+  final List<String> steps;
 
+
+  const Recipe({
+    //required this.id,
+    required this.recipeName,
+    required this.imageUrl,
+    required this.prepTime,
+    required this.complexity,
+    required this.affordability,
+    required this.ingredients,
+    required this.steps,
+ });
+} //class
+
+enum Complexity { Simple, Challenging, Hard, }
+
+enum Affordability {
+  $,
+  $$,
+  $$$,
+}
